d46d58fcd520c071d3e432fc81b2722edcdf6d0a
Tung Nguyen <tungnguyen@Tungs-MacBook-Pro.local>
2025-03-02 17:53:33 +0700

    feat: implement cicd flow and get uploaded files endpoint

--- server/src/modules/crawler/services/crawler.service.ts
+++ server/src/modules/crawler/services/crawler.service.ts
@@ -4,33 +4,34 @@ import {
   Injectable,
   InternalServerErrorException,
   Logger,
-  OnModuleDestroy,
-  OnModuleInit,
 } from '@nestjs/common';
-import { Browser, Page } from 'puppeteer';
+import { Page, executablePath } from 'puppeteer';
 import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import UserAgent from 'user-agents';
 puppeteer.use(StealthPlugin());
 
 @Injectable()
-export class CrawlerService implements OnModuleInit, OnModuleDestroy {
+export class CrawlerService {
   private readonly logger = new Logger(CrawlerService.name);
 
-  private browser: Browser;
-
   public async crawlGoogle(keyword: string): Promise<CrawledGoogleResponse> {
-    if (!this.browser) {
-      throw new InternalServerErrorException('Cannot launch browser!');
-    }
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: [
+        '--disable-setuid-sandbox',
+        '--ignore-certificate-errors',
+        '--no-sandbox',
+      ],
+      executablePath: executablePath() || '/usr/bin/google-chrome',
+    });
 
     const generateUserAgent = new UserAgent();
 
-    const page = await this.browser.newPage();
+    const page = await browser.newPage();
 
     try {
       await page.setUserAgent(generateUserAgent.toString());
-      await page.setViewport(GoogleCrawlerOption.viewPort);
       await page.setExtraHTTPHeaders({
         'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
       });
@@ -49,6 +50,8 @@ export class CrawlerService implements OnModuleInit, OnModuleDestroy {
       } catch (selectorError) {
         await this.detectCaptcha(page);
 
+        await page.close();
+        await browser.close();
         throw selectorError;
       }
 
@@ -70,6 +73,7 @@ export class CrawlerService implements OnModuleInit, OnModuleDestroy {
       ]);
 
       await page.close();
+      await browser.close();
 
       return {
         content: htmlContent,
@@ -78,35 +82,14 @@ export class CrawlerService implements OnModuleInit, OnModuleDestroy {
       };
     } catch (error) {
       await page.close();
-      this.logger.error(`CrawlerService crawlGoogle Error: ${error}`);
+      await browser.close();
+      this.logger.error(
+        `CrawlerService crawlGoogle Error: ${JSON.stringify(error, null, 5)}`,
+      );
       throw new InternalServerErrorException(
         `Crawl ${keyword} keyword failed: ${error.message}`,
       );
     }
-  }
-
-  async onModuleDestroy() {
-    if (this.browser) {
-      await this.browser.close();
-    }
-  }
-
-  async onModuleInit() {
-    this.browser = await puppeteer.launch({
-      headless: true,
-      executablePath: puppeteer.executablePath(),
-      args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-dev-shm-usage',
-        '--disable-gpu',
-        '--single-process',
-        '--incognito',
-        '--disable-client-side-phishing-detection',
-        '--disable-software-rasterizer',
-      ],
-      ignoreHTTPSErrors: true,
-    });
   }
 
   private async detectCaptcha(page: Page) {
@@ -117,7 +100,6 @@ export class CrawlerService implements OnModuleInit, OnModuleDestroy {
     );
     if (isCaptchaPresent) {
       this.logger.warn('CAPTCHA detected!');
-      await page.close();
       throw new InternalServerErrorException('Google CAPTCHA detected');
     }
   }
